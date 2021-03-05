8d48a66b4cc340e19c2f6e165efa49c47a117434
webVue <15115359055@139.com>
2021-03-05 16:03:51 +0800

    new config

---
+++ src/router/config/index.js
@@ -0,0 +1,12 @@
+/**
+ * router config 自动生成
+ * title	    页面title / 左侧sidebar
+ * name	        使用 Symbol 确保唯一性，若未设置则默认为随机字符串
+ * type	        folder：有子路由，折叠sidebar / tab：子路由在右侧以menuTab展现 / view：直接展示页面
+ * icon	        可直接配置 iconfont 类名 / 也可配置为图片路径
+ * filePath	    文件路径
+ * route	    当前页面路由
+ * order	    路由排序
+ * inNav	    是否在左侧菜单栏显示
+ * permission	当前路由权限，数组格式，满足数组内任一权限即可展示该页面
+ */
